9fca1cd7646cfe09f1b84d657908fa59223347b2
Sorin Ilin <sorin@Sorin-desktop>
2024-01-01 21:45:33 +0200

    add getters test

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -3,16 +3,22 @@
 namespace App\Tests\Unit\Entity;
 
 use PHPUnit\Framework\TestCase;
+use App\Entity\Dinosaur;
 
 class DinosaurTest extends TestCase
 {
-    public function testItWorks(): void
+    public function testCanGetAndSetData(): void
     {
-        self::assertEquals('42', 42);
-    }
+        $dino = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A'
+        );
 
-    public function testItWorksTheSame(): void
-    {
-        self::assertSame('42', 42);
+        self::assertSame('Big Eaty', $dino->getName());
+        self::assertSame('Tyrannosaurus', $dino->getGenus());
+        self::assertSame(15, $dino->getLength());
+        self::assertSame('Paddock A', $dino->getEnclosure());
     }
 }
