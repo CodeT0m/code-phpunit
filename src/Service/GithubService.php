6796761a1216173cfe0ead4b750435e65276ca9b
Sorin Ilin <sorin@Sorin-desktop>
2024-01-02 13:29:45 +0200

    implement github service method

--- src/Service/GithubService.php
+++ src/Service/GithubService.php
@@ -3,15 +3,46 @@
 namespace App\Service;
 
 use App\Enum\HealthStatus;
+use Symfony\Component\HttpClient\HttpClient;
 
 class GithubService
 {
     public function getHealthReport(string $dinosaurName): HealthStatus
     {
-        // Call Github API
+        $health = HealthStatus::HEALTHY;
+        
+        $client = HttpClient::create();
 
-        // Filter the issues
+        $response = $client->request(
+            method: 'GET',
+            url: 'https://api.github.com/repos/SymfonyCasts/dino-park/issues'
+        );
 
-        return HealthStatus::HEALTHY;
+        foreach ($response->toArray() as $issue) {
+            if (str_contains($issue['title'], $dinosaurName)) {
+                $health = $this->getDinoStatusFromLabels($issue['labels']);
+            }
+        }
+
+        return $health;
+    }
+
+    private function getDinoStatusFromLabels(array $labels): HealthStatus
+    {
+        $status = null;
+
+        foreach ($labels as $label) {
+            $label = $label['name'];
+
+            // We only care about "Status" labels
+            if (!str_starts_with($label, 'Status:')) {
+                continue;
+            }
+
+            // Remove the "Status:" and whitespace from the label
+            $status = trim(substr($label, strlen('Status:')));
+        }
+
+        return HealthStatus::tryFrom($status);
     }
 }
